e28f478b5d6453de25d645ac0eda4fcf94673169
Ian S Pringle <ipringle@protonmail.com>
2018-10-18 00:51:20 -0400

    Updated help text, corrected spacing, swapped '"' to "'"

--- legos/bible.py
+++ legos/bible.py
@@ -16,23 +16,25 @@ class Bible(Lego):
 			logger.error('''Could not identify message source in message:
 						{}'''.format(str(message)))
 
-		base_url = "https://bible-api.com/"
-		passage = message['text'].replace(message['text'].split()[0], '').strip()
+		base_url = 'https://bible-api.com/'
+		passage = message['text'].replace(
+					message['text'].split()[0], '').strip()
 
 		r = requests.get(base_url + passage)
 
 		if r.status_code == requests.codes.ok:
 			reference = r.json()['reference']
 			text = r.json()['text']
-			self.reply(message, reference + ":\n" + text, opts)
+			self.reply(message, reference + ':\n' + text, opts)
 		else:
 			logger.erro('Requests encountered an error.')
 			logger.error('''HTTP GET response code:
 						{}'''.format(r.status_code))
 
 	def get_help(self):
-		help_text = "Get Bible references using bible-api.com. " \
-					"All text uses the WEB translation. " \
-					"Usage: !bible <book> <chapter>:<verse(s)>"
+		help_text = ('Get Bible references using bible-api.com. ' 
+					'All text uses the WEB translation. ' 
+					'Usage: !bible <book> <chapter>:<verse(s)>'
+					'Checkout github.com/pard68/legos.bible for more info')
 		return help_text
 
